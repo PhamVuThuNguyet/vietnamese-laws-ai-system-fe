43c4e739169059718a50d0c175542e0f7fefe53f
hoangtrung1801 <hoangtrung1801.2003@gmail.com>
2023-11-29 22:48:33 +0700

    fix: cicd

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,7 +76,7 @@ module.exports = {
       },
     ],
     //#endregion  //*======== Import Sort ===========
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
   globals: {
     React: true,
